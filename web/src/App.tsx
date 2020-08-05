0ee0e2c07d37eb676c346b8e1871cec3e83e1b9a
MCoutinho <matheuscavalcante88@hotmail.com>
2020-08-05 18:33:01 -0300

    web: add landing, teacher list, form pages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-import Landing from './pages/Landing'
+import Routes from './routes';
 
 import './assets/styles/global.css';
 
 function App() {
   return (
-    <Landing />
+    <Routes />
   );
 }
 
 export default App;
+//1:48:08 1
